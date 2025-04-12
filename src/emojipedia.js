38582d9efa86e2186cbd7902b705ef32cdbd194b
yehiaakarim <yehiakarim1994@gmail.com>
2025-04-12 03:05:10 +0200

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,116 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits.",
+  },
+  {
+    id: 5,
+    emoji: "😟",
+    name: "Worried Face",
+    meaning:
+      "The worried face reflects tension in the face of a complicated situation or important event. Conveys anxiety, uncertainty, and discomfort. It is associated with many moderately sad or tense emotions, including worry, fear, panic and disappointment.",
+  },
+  {
+    id: 6,
+    emoji: "😡",
+    name: "Pouting Face",
+    meaning:
+      "The pouting face expresses displeasure with a person or situation. Beware, danger of explosion! Furious emoji face. Is annoyed and already sulking with a deep-red face.",
+  },
+  {
+    id: 7,
+    emoji: "😎",
+    name: "Face With Sunglasses",
+    meaning:
+      "It expresses coolness and confidence and is used to convey a carefree, relaxed attitude or to express that something is excellent. If you are confident in yourself, have done something particularly well and are proud of it, this emoji is an appropriate choice. It is also a symbol of sunshine, summer vacations, and beach vacations.",
+  },
+  {
+    id: 8,
+    emoji: "😍",
+    name: "Smiling Face With Heart-Eyes",
+    meaning:
+      "A smile of love is shown by the emoji, which beams with happiness with its heart-shaped eyes. It conveys intense love and affection for a person, place or even an object. This emoji is often used in romantic messages to express love, gratitude, and admiration. It can also be used to convey enthusiasm for a cause or event. The heart-shaped eyes symbolize overwhelming feelings of happiness and a strong connection.",
+  },
+  {
+    id: 9,
+    emoji: "😇",
+    name: "Smiling Face With Halo",
+    meaning:
+      "People use it to express their good deeds, loving behavior or exemplary attitude. Sometimes this emoji is used humorously to show a winking innocence for less good deeds or behaviors. It conveys kindness and goodwill.",
+  },
+  {
+    id: 10,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "I'm not serious! The inverted smiling face indicates that you are not serious or have a humorous undertone. It is suitable for ambiguous statements, ironic remarks or joking comments. Also, it can be used to express silliness or in response to a joke.",
+  },
+  {
+    id: 11,
+    emoji: "😋",
+    name: "Face Savouring Delicious Food",
+    meaning:
+      "Bon appetit! Licking your lips with pleasure, the 😋 emoji shows an expression of satisfaction, hunger or joy over delicious food. It is shown beaming with joy when you have cooked something particularly tasty or when your plate is empty.",
+  },
+  {
+    id: 12,
+    emoji: "😝",
+    name: "Face With Stuck-Out Tongue & Tightly Closed Eyes",
+    meaning:
+      "Users can use this emoji, which is a winking face with its tongue stuck out, to playfully express their feelings by making fun of, getting angry at, or jokingly insulting someone. This emoji is often used for provocative messages, crude or black humor, and gloating.",
+  },
+  {
+    id: 13,
+    emoji: "🧐",
+    name: "Face With Monocle",
+    meaning:
+      "With its monocle, this emoji represents a face that was considered a status symbol in the 19th century. It shows furrowed eyebrows and a slightly raised head, as if examining something closely. Used to indicate that something seems suspicious to you, or that you are making a skeptical or ironic observation.",
+  },
+  {
+    id: 14,
+    emoji: "😒",
+    name: "unamused Face",
+    meaning:
+      "This emoji is a face with slightly raised eyebrows, a frown, and a sideways look that shows dissatisfaction and displeasure. It is often used to express disinterest or disapproval of a person or situation. Equally, it can be used to communicate that you are not enthusiastic about something and want to share your dissatisfaction.",
+  },
+  {
+    id: 15,
+    emoji: "😤",
+    name: "Face With Look of Triumph",
+    meaning:
+      "i did it, Pride, anger and contempt. White steam clouds are coming out of the smiley's nose. Is snorting contemptuously as a sign of superiority. Is proud and triumphing over someone else.",
+  },
+  {
+    id: 16,
+    emoji: "😴",
+    name: "Sleeping Face",
+    meaning:
+      "Good night and sweet dreams! In comics or cartoons, zZz above the head stands for sleeping characters. The emoji is tired, wants to go to bed or is just about to fall asleep from boredom.",
+  },
+];
+
+export default emojipedia;
